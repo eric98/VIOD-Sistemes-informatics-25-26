8374394d94a2898768a567cda26cb09eddc541e2
ericg <ergare.17@gmail.com>
2026-01-14 22:48:07 +0100

    Fixed bugs

--- 7-1/client.sh
+++ 7-1/client.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-# b) El client inicia la connexió i envia la capçalera VIOD-NET per indicar que vol connectar-se.
-SERVER_IP="10.65.0.53"
+SERVER_IP="192.168.1.57"
 SERVER_PORT=50000
 CLIENT_PORT=51000
 
 FILE_NAME="enviament.txt"
 
-echo "VIOD-NET" | nc $SERVER_IP $SERVER_PORT
+# a)
+
+# b) El client inicia la connexió i envia la capçalera VIOD-NET per indicar que vol connectar-se.
+echo "b)"
+echo "VIOD-NET" | nc -q 0 $SERVER_IP $SERVER_PORT
+
+# c)
 
 # d) El client llegeix la resposta del servidor:
 #  • si rep KO_HEADER, tanca el programa.
 #  • si rep OK_HEADER, envia un missatge amb la capçalera FILE_NAME i el nom d’un fitxer a enviar.
-message=$(nc -l $CLIENT_PORT)
-
+echo "d) escolto OK_HEADER"
+message=$(nc -l -p $CLIENT_PORT)
+echo "rebut $message"
 # Es tanca el programa per a qualsevol capçalera diferent a "OK_HEADER
-if [ $message != "OK_HEADER" ]
+if [ $message != "OK_HEADER" ]; then
         exit 1 # Sortida amb un error general
+fi
 
-echo "FILE_NAME" | nc $SERVER_IP $SERVER_PORT
+echo "envio FILE_NAME"
+# TODO: enviar el nom del fitxer
+echo "FILE_NAME" | nc -q 0 $SERVER_IP $SERVER_PORT
+
+# e)
 
 # f) El client espera rebre la resposta del servidor:
 #  • si no rep OK_FILE_NAME, tanca el programa.
 #  • si rep OK_FILE_NAME, envia el contingut del fitxer mitjançant la comanda cat.
-message=$(nc -l $CLIENT_PORT)
+echo "f)"
+message=$(nc -l -p $CLIENT_PORT)
 
-if [ $message != "OK_FILE_NAME" ]
+if [ $message != "OK_FILE_NAME" ]; then
         exit 1 # Sortida amb un error general
+fi
 
-cat $FILE_NAME | nc $SERVER_IP $SERVER_PORT
+cat $FILE_NAME | nc -q 0 $SERVER_IP $SERVER_PORT
 
 exit 0 # Sortida satisfactòria (sense errors)
--- 7-1/server.sh
+++ 7-1/server.sh
@@ -2,35 +2,47 @@
 
 SERVER_PORT=50000
 CLIENT_PORT=51000
-CLIENT_IP="10.65.0.53"
+CLIENT_IP="192.168.1.82"
 
 FILE_NAME="rebuda.txt"
 
 # a) El servidor escolta connexions en un port TCP definit prèviament
+echo "a)"
 message=$(nc -l $SERVER_PORT)
+
+# b)
 
 # c) El servidor verifica la capçalera rebuda:
 #   • si la capçalera no és VIOD-NET, el servidor respon amb KO_HEADER i tanca la connexió.
 #   • si la capçalera és VIOD-NET, el servidor respon amb OK_HEADER i continua l’execució.
-if [ $message != "VIOD-NET" ]
-	echo "KO_HEADER" | nc $CLIENT_IP $CLIENT_PORT
+echo "c) Rebut $message"
+if [ $message != "VIOD-NET" ]; then
+	echo "KO_HEADER" | nc -q 0 $CLIENT_IP $CLIENT_PORT
 	exit 1 # Sortida amb un error general
+fi
 
-echo "OK_HEADER" | nc $CLIENT_IP $CLIENT_PORT
+echo "envio OK_HEADER"
+echo "OK_HEADER" | nc -q 0 $CLIENT_IP $CLIENT_PORT
+
+# d)
 
 # e) El servidor llegeix el missatge rebut:
 #   • si la capçalera no és FILE_NAME, respon amb KO_FILE_NAME i tanca la connexió.
 #   • si la capçalera és FILE_NAME, respon amb OK_FILE_NAME.
+echo "e) Escolto nom del fitxer"
 message=$(nc -l $SERVER_PORT)
-
-if [ $message != "FILE_NAME" ]
-        echo "KO_FILE_NAME" | nc $CLIENT_IP $CLIENT_P
-ORT
+echo "rebut $message"
+if [ $message != "FILE_NAME" ]; then
+        echo "KO_FILE_NAME" | nc -q 0 $CLIENT_IP $CLIENT_PORT
         exit 1 # Sortida amb un error general
+fi
 
-echo "OK_FILE_NAME" | nc $CLIENT_IP $CLIENT_PORT
+echo "OK_FILE_NAME" | nc -q 0 $CLIENT_IP $CLIENT_PORT
+
+# f)
 
 # g) El servidor llegeix el missatge rebut i ho guarda al fitxer $FILE_NAME
+echo "g)"
 message=$(nc -l $SERVER_PORT)
 
 echo $message >> $FILE_NAME
